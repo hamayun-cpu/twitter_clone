ba7db4e47554302cabcc040917869d6f418685bb
hamayun <hamayun.waheed1999@gmail.com>
2021-01-07 03:45:17 +0500

    added registration controller, some new view templates and a migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :username
+
+  has_many :tweets
 end
